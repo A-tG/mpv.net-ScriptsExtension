ebee4146acb9e9d4437d7a9803ed3dbccca4497c
A-tG <a3tg.developer@gmail.com>
2022-04-11 21:26:13 +0300

    replaced Marshal.WriteByte

--- AtgScriptsExtension/Extensions/BitmapExtensions.cs
+++ AtgScriptsExtension/Extensions/BitmapExtensions.cs
@@ -8,7 +8,7 @@ namespace AtgScriptsExtension.Extensions
 {
     static internal class BitmapExtensions
     {
-        static internal void Read32RgbFromPaddedRgb0(this Bitmap bmp, int strideB, IntPtr data, int lenB)
+        unsafe static internal void Read32RgbFromPaddedRgb0(this Bitmap bmp, int strideB, IntPtr data, int lenB)
         {
             const int bpp = 4; // r, g, b, X - 4 bytes
             var format = bmp.PixelFormat;
@@ -27,11 +27,12 @@ namespace AtgScriptsExtension.Extensions
             var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
             var bmData = bmp.LockBits(rect, ImageLockMode.ReadWrite, format);
 
-            var readPtr = data;
-            var writePtr = bmData.Scan0;
+            var readPtr = (byte*)data;
+            var writePtr = (byte*)bmData.Scan0;
             var pixelsNumber = (lenB - paddingB) / bpp;
             var strideP = strideB / bpp;
             var widthP = bmp.Width;
+
             Parallel.For(0, pixelsNumber, (pIndex) =>
             {
                 int y = pIndex / strideP;
@@ -40,14 +41,13 @@ namespace AtgScriptsExtension.Extensions
                 if (isOutOfBound) return;
 
                 int ofs = bpp * pIndex;
-                var rPtr = readPtr + ofs;
-                var wPtr = writePtr + ofs - paddingB * y;
-                const int gOfs = 1;
-                const int bOfs = 2;
-                Marshal.WriteByte(wPtr, Marshal.ReadByte(rPtr));
-                Marshal.WriteByte(wPtr, gOfs, Marshal.ReadByte(rPtr + gOfs));
-                Marshal.WriteByte(wPtr, bOfs, Marshal.ReadByte(rPtr + bOfs));
+                for (int i = 0; i < 3; i++)
+                {
+                    int outOfBoundOfs = paddingB * y;
+                    writePtr[ofs + i - outOfBoundOfs] = readPtr[ofs + i];
+                }
             });
+
             bmp.UnlockBits(bmData);
         }
     }
