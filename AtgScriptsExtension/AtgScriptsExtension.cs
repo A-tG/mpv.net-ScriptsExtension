4f52565f14cbe4c942978fd51d7ec03efccec3e0
A-tG <a3tg.developer@gmail.com>
2022-04-12 11:19:03 +0300

    Update AtgScriptsExtension.cs

--- AtgScriptsExtension/AtgScriptsExtension.cs
+++ AtgScriptsExtension/AtgScriptsExtension.cs
@@ -7,7 +7,7 @@ namespace AtgScriptsExtension
     public class AtgScriptsExtension : IExtension
     {
         // Add to input.conf
-        // Ctrl+c script-message atg_screenshot-to-clipboard
+        // Ctrl+c script-message atg_screenshot-to-clipboard [flags]
         ScreenshotToClipboardScript screenshotToClipboardScript = new ScreenshotToClipboardScript();
     }
 }
